75902a20aa0ca4a1fbeb9291ba2ab4947a20b423
guntukuaravind <aravind.guntuku1707@gmail.com>
2021-12-13 12:57:46 +0530

    3 files modified

--- src/app/-services/user.service.ts
+++ src/app/-services/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-const API_URL = 'http://localhost:8085/api/test/';
+const API_URL = 'http://localhost:8090/api/test/';
 @Injectable({
   providedIn: 'root'
 })
